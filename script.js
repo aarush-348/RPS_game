ffee80ae14f88076ac4d38892e77ad16afe7cba5
aarush-348 <72818329+aarush-348@users.noreply.github.com>
2025-01-09 15:38:19 +0000

    Final game completed

--- script.js
+++ script.js
@@ -8,34 +8,94 @@
 
 function getComputerChoice()
 {
-    return Math.random() * 3
+    return Math.random() * 3;
+
 }
 
 function getHumanChoice()
 {
-    let choice = prompt("Rock, Paper, Scizzors!")
+    let choice = prompt("Rock, Paper, Scizzors! (R, P or S)");
 
-    if(choice.toLowerCase() == "rock" ||  ||  )
-    {
-        return 0
-    }
+    if(choice.toLowerCase() == "r")
+        return 0;
 
-    else if (choice.toLowerCase() == "paper")
-    {
-        return 1
-    }
+    else if (choice.toLowerCase() == "p")
+        return 1;
+    
 
-    else if (choice.toLowerCase() == "scizzor")
-    {
-        return 2
-    }
+    else if (choice.toLowerCase() == "s")
+        return 2;
+    
 
     else 
     {
-        getHumanChoice()
+        alert("Incorrect choice")
+        getHumanChoice();
     }
 }
 
-getHumanChoice()
+function playRound(humanChoice, computerChoice)
+{
+    if (computerChoice == 0)
+        alert("The computer's choice is Rock! " +  winDecider(humanChoice, computerChoice))
 
-if ()
+    if (computerChoice == 1)
+        alert("The computer's choice is Paper! " +  winDecider(humanChoice, computerChoice))
+
+    if (computerChoice == 2)
+        alert("The computer's choice is Scizzors! " +  winDecider(humanChoice, computerChoice))
+}
+
+
+function winDecider(humanChoice, computerChoice)
+{
+    if ((humanChoice == 0 && computerChoice == 2) || (humanChoice == 1 && computerChoice == 0 ) || (humanChoice == 2 && computerChoice == 1 ))
+    {
+        
+        humanScore++
+        return "You won!"
+    }
+
+    else if (humanChoice == computerChoice)
+        return "It's a tie!"
+
+    else 
+    {
+        computerScore++
+        return "Computer Won!"
+    }
+}
+
+function playGame()
+{
+    for(let i = 0; i < 5; i++)
+    {
+        const humanSelection = getHumanChoice();
+        const computerSelection = Math.trunc(getComputerChoice());
+
+        playRound(humanSelection, computerSelection);
+    }
+}
+
+function result(humanScore, computerScore)
+{
+    console.log("Your score = " + humanScore)
+    console.log("Computer score = " + computerScore)
+
+    if (computerScore > humanScore)
+        alert("Computer won the game!")
+
+    else if (computerScore == humanScore)
+        alert("The game is a Tie!")
+
+    else if (humanScore > computerScore)
+        alert("You won the game!")
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playGame()
+result(humanScore, computerScore)
+
+
